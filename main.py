15a162fabf6f64606daf3558555c374e09de85ff
Satya Mrudula <150340214+satya-mrudula@users.noreply.github.com>
2026-09-12 09:41:05 +0530

    Change file path for root response

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 def root():
-    return FileResponse("webpage/index.html")
+    return FileResponse("static/index.html")
 
 
 @app.get("/health")
